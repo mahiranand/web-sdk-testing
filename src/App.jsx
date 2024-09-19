6e15893737ff6aafeb905197eb5e4e80eca8d2f0
mahiranand <mahiranand08@gmail.com>
2024-09-19 14:15:36 +0530

    third commmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
 // import {CustomerGluComponent} from "@customerglu/react-web-sdk";
 import "./styles.css";
-// import { CustomerGluComponent } from "@customerglu/react-web-sdk";
+import { CustomerGluComponent } from "@customerglu/react-web-sdk";
 
-import CustomerGluComponent from "./component/CustomerGluComponent";
+// import CustomerGluComponent from "./component/CustomerGluComponent";
 import { Route, Routes } from "react-router-dom";
 import Cgtest from "./component/Cgtest";
 import Error from "./component/Error";
 
 function Main() {
-  <div className="App">
-    <div
-      id="embedId"
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        cursor: "pointer",
-      }}
-    >
-      <CustomerGluComponent
-        gluToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJnbHV0ZXN0LTI1OCIsImdsdUlkIjoiNmUwZTUwZDItMzQ1Mi00YTE1LTljMTMtM2YzZTJmZDA2NGQ3IiwiY2xpZW50IjoiN2IxOTEwYTQtNzMxYS00MmY2LTkyODQtMmU2NGRhNGM5MTRkIiwiZGV2aWNlSWQiOiJnbHV0ZXN0LTI1OF9kZWZhdWx0IiwiZGV2aWNlVHlwZSI6ImRlZmF1bHQiLCJpc0xvZ2dlZEluIjp0cnVlLCJpYXQiOjE3MjQ3NDEwOTksImV4cCI6MTc1NjI3NzA5OX0.q4CnB-foMnkVsQQgm7ijK8YcxskDLENDNTkwtgrZztQ"
-        userId="glutest-258"
-        region="us"
+  return (
+    <div className="App">
+      <div
+        id="embedId"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          cursor: "pointer",
+        }}
       >
-        <div></div>
-      </CustomerGluComponent>
+        <CustomerGluComponent
+          gluToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJnbHV0ZXN0LVNlcDMyMjIxIiwiZ2x1SWQiOiJjYzhlZTY5Ny1lNjczLTRjNzAtYTI4NC1kMzhlOGNkNzU1N2EiLCJjbGllbnQiOiIxNzA1OGIxNS1hYzc0LTQ4NjktYjlhNC1iOTJkNjRlMWI3NzUiLCJkZXZpY2VJZCI6ImdsdXRlc3QtU2VwMzIyMjFfZGVmYXVsdCIsImRldmljZVR5cGUiOiJkZWZhdWx0IiwiaXNMb2dnZWRJbiI6dHJ1ZSwiaWF0IjoxNzI2NzMzMDc5LCJleHAiOjE3NTgyNjkwNzl9.F_a2yW9V6F3dD-4hBdG5kbNgp6oPdT1A3lPlq0mov3M"
+          userId="glutest-Sep32221"
+          region="us"
+        >
+          <div></div>
+        </CustomerGluComponent>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default function App() {
@@ -54,7 +56,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/test" element={<Cgtest />} />
-        <Route path="*" element={<Error /> } />
+        <Route path="*" element={<Error />} />
       </Routes>
     </>
   );
